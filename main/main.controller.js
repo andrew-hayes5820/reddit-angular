0afa85630975f5f9e15b94966a9b2f8e1586e0dd
Andrew Hayes <andy.hayes13@gmail.com>
2017-08-01 21:12:57 -0400

    updated all the things

--- main/main.controller.js
+++ main/main.controller.js
@@ -1,13 +1,12 @@
 var app = angular.module ('reddit');
 
-app.controller('MainCtrl', function($scope, $http){
-// use http service to get data from Reddit (can pick any other URL)
-  $http.get('https://www.reddit.com/r/aww/.json')
-    .then(function(result){
-      // above function effictively stores the resulting http GET request in a variable name.
-      // the word 'result' after 'function' can be substitued for just about any word (with the exception
-      // of JavaScript keyword)
-      // console.log(result);
-      $scope.posts = result.data.data.children;
-    });
+app.controller('MainCtrl', function($scope, $timeout, RedditFactory){
+
+  RedditFactory.getPosts()
+  .then(function(result){
+    $timeout($scope.posts = result);
+  })
+  .catch(function(error){
+    $scope.error = 'there was an error getting posts';
+  });
 });
